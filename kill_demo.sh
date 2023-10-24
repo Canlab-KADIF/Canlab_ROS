7873d1e65137eba98dd725dd14bf12e4087ac96f
TaeJung <rlatae123@can-lab.co.kr>
2023-10-24 14:32:37 +0900

    kill_demo.sh add rosbag

--- kill_demo.sh
+++ kill_demo.sh
@@ -6,10 +6,16 @@ if [ `whoami` == "root" ]; then
 	killall -2 roslaunch
 	killall -2 nodelet
 	killall -2 socketcan_bridg
+	if [ `pgrep -l rosbag | awk '{print $2}'` == "rosbag" ]; then
+		killall -2 rosbag
+	fi
 else
 	echo ${PASSWD} | sudo -S killall -2 roslaunch
 	echo ${PASSWD} | sudo -S killall -2 nodelet
 	echo ${PASSWD} | sudo -S killall -2 socketcan_bridg
+	if [ `pgrep -l rosbag | awk '{print $2}'` == "rosbag" ]; then
+		echo ${PASSWD} | sudo -S killall -2 rosbag
+	fi
 fi
 
 echo ' '
